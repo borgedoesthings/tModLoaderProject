13d2f91b257f7603a465ed28b73cf2385d3208e4
Coty Angelopolus <98544052+coty-angelopolus@users.noreply.github.com>
2025-04-09 13:25:00 -0400

    changed update() to allow for redirect after audio support fails and also added a testing file to test my audio change

---
+++ test/SoundEngineTesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Terraria.Audio;
+
+namespace Terraria.ModLoader
+{
+	[TestClass]
+	public class SoundEngineTests
+	{
+		[TestMethod]
+		public void TestAudioSupport()
+		{
+			var method = typeof(SoundEngine).GetMethod("TestAudioSupport", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+			bool result = (bool)method.Invoke(null, null);
+
+			// Just confirm it runs and returns a bool and not crash
+			Assert.IsTrue(result || !result);
+		}
+	}
+}
