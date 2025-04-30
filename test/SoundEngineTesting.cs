e33528f0574a3296ead415b0f6b5cdbacc118794
Coty Angelopolus <98544052+coty-angelopolus@users.noreply.github.com>
2025-04-30 10:09:57 -0400

    Changed the update(0 in ActiveSound.cs, SoundPlayer.cs, and SoundEngine.cs to log audio states when the update(0 function is called within these files

--- test/SoundEngineTesting.cs
+++ test/SoundEngineTesting.cs
@@ -5,20 +5,47 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Terraria.Audio;
+using System.Reflection;
+//using static Terraria.GameContent.Bestiary.BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions;
+using Moq;
+using ReLogic.Utilities;
 
 namespace Terraria.ModLoader
 {
-	[TestClass]
-	public class SoundEngineTests
-	{
-		[TestMethod]
-		public void TestAudioSupport()
-		{
-			var method = typeof(SoundEngine).GetMethod("TestAudioSupport", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
-			bool result = (bool)method.Invoke(null, null);
+    [TestClass]
+    public class SoundEngineTests
+    {
+        [TestMethod]
+        public void TestAudioSupport()
+        {
+            var method = typeof(SoundEngine).GetMethod("TestAudioSupport", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            bool result = (bool)method.Invoke(null, null);
 
-			// Just confirm it runs and returns a bool and not crash
-			Assert.IsTrue(result || !result);
-		}
-	}
+            // Just confirm it runs and returns a bool and not crash
+            Assert.IsTrue(result || !result);
+        }
+
+        [TestMethod]
+        public void PauseAll_happypath()
+        {
+            var soundPlayer = new SoundPlayer();
+
+            soundPlayer.PauseAll(); // no errors should be thrown wether the sound is playing or not
+        }
+
+        [TestMethod]
+        public void ResumeAll_Happypath()
+        {
+            var soundPlayer = new SoundPlayer();
+
+            soundPlayer.ResumeAll(); 
+        }
+
+        [TestMethod]
+        public void SoundEngine_Update_ResumesAudioWhenGameUnpaused()
+        {
+            //content
+        }
+
+    }
 }
